ca57048dafbb188df62d767290679e7d30bbc4eb
Pea88 <159432576+Pea88@users.noreply.github.com>
2025-03-05 14:29:45 +0000

    code changes

--- src/main/java/com/example/ecommerce/App.java
+++ src/main/java/com/example/ecommerce/App.java
@@ -1,4 +1,4 @@
-package org.learn.ecommerce;
+package com.example.ecommerce;
 
 /**
  * Hello world!
---
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.controller;
+import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+}
